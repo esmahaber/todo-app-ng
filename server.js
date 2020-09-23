cd24cb27511182531a167ac4d035014392fd2d38
esmahaber <l1621012063@stud.sdu.edu.tr>
2020-09-23 22:39:30 +0300

    updated start script

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ const PORT = process.env.PORT || 5000;
 var app_path = './dist/todoApp';
 
 app.use('/', express.static(path.join(__dirname, app_path)))
+.get('*', (req,res) => res.sendFile(path.join(__dirname, app_path +'/index.html')))
 .listen(PORT, ()=> console.log(`Listening on ${PORT}`));
